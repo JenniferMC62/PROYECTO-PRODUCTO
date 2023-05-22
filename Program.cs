e351b047aee5cd4666c58b569df0e3495f575655
Jennifer Macedo <126447294+JenniferMC62@users.noreply.github.com>
2023-05-22 19:25:22 +0000

    proceso

---
+++ Program.cs
@@ -0,0 +1,22 @@
+class Product
+{
+    public string code;
+    public string description;
+    public decimal price;
+
+    public Product(string code, string description, decimal price)
+    {
+        this.code = code;
+        this.description = description;
+        this.price = price;
+    }
+
+}
+
+class ProductDB
+{
+    static void SaveProducts(List<Product> products)
+    {
+        StreamWriter
+    }
+}
